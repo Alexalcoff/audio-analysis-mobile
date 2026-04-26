ea8fe7c6756292ff832b0250f871b04463936403
Alexalcoff <alexalcoff55@gmail.com>
2026-04-26 20:46:30 +0500

    прода

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
@@ -7,13 +7,13 @@ def home():
     return {"status": "server running"}
 
 @app.post("/recognize")
-async def recognize(file: UploadFile):
-
+async def recognize(file: UploadFile = File(...)):
     audio = await file.read()
+
+    print(f"Received file size: {len(audio)} bytes")
 
     return {
         "title": "Test Song",
         "artist": "Test Artist",
         "youtube_link": "https://music.youtube.com/watch?v=test"
     }
-
