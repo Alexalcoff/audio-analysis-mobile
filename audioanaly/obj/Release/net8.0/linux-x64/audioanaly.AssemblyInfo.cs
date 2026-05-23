a186c51531ea2f0d92f707a09727fbc3f78647ab
Alexalcoff <alexalcoff55@gmail.com>
2026-05-23 18:40:14 +0500

    iutfoliuhgo;ihj

--- audioanaly/obj/Release/net8.0/linux-x64/audioanaly.AssemblyInfo.cs
+++ audioanaly/obj/Release/net8.0/linux-x64/audioanaly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("audioanaly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5438830f30b528213b236bfdd4819458af5ab5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f373e21b6db20e141b23866f0a742dc0a293e2a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("audioanaly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("audioanaly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
