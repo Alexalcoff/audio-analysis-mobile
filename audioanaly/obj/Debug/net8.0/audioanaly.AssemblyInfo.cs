d25a958a8f1d79aca9555c2829e415be52851da0
Alexalcoff <alexalcoff55@gmail.com>
2026-05-23 11:22:42 +0500

    srfh

--- audioanaly/obj/Debug/net8.0/audioanaly.AssemblyInfo.cs
+++ audioanaly/obj/Debug/net8.0/audioanaly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("audioanaly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d50f7f7e85c796bf983a61659ed2f09da818ded")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc942fa46f3798076fc1e5d314c7c68241b84c31")]
 [assembly: System.Reflection.AssemblyProductAttribute("audioanaly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("audioanaly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
